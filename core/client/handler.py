8c6bc3043c383d323f9c13b0e9c4d07d4952e590
LeftandRight <leftandright.x@gmail.com>
2025-02-15 23:28:12 +0800

    generator support

--- core/client/handler.py
+++ core/client/handler.py
@@ -1,10 +1,11 @@
-import socket, os, importlib
-import json, typing, contextlib
+import socket, typing, threading
 
 from ..enums import PacketType
+from ..commands import loadCommand
 
 if typing.TYPE_CHECKING:
     from .connect import Connect
+
 
 class serverHandler:
     def __init__(self, socketInstance: socket.socket) -> None:
@@ -14,29 +15,32 @@ class serverHandler:
         if isinstance(response, bytes):
             response = response.decode("utf-8")
 
-        fileList = [fileName for fileName in os.listdir("core/command") if fileName.endswith(".py")]
-        moduleList = {
-            json.dumps(getattr((mod := importlib.import_module("core.command." + fileName[:-3])), "__aliases__", [])):
-                getattr(mod, "on_client_receive", lambda: ...)
-
-            for fileName in fileList
-        }
-
-        for fn, func in moduleList.items():
-            if ((_splitted_resp := response.split(' '))[0] not in json.loads(fn)):
+        for commandFunction in loadCommand():
+            if (splittedResponse := response.split(" "))[0] not in commandFunction.__aliases__:
                 continue
+            if commandFunction._generatorFunction:
 
-            try:
-                returnValue = func(self._socketInstance, *_splitted_resp[1:])
+                def runGenerator() -> None:
+                    for data in commandFunction().on_client_receive(self._socketInstance):
+                        self._socketInstance.send_(packetType=PacketType.COMMAND_RESPONSE, data=data)
 
-            except TypeError:
-                returnValue = func(self._socketInstance)
+                threading.Thread(target=runGenerator).start()
+                break
 
-        if (returnValue):
-            with contextlib.suppress(Exception):
-                returnValue = bytes(returnValue, 'UTF-8')
+            self._socketInstance.send_(packetType=PacketType.COMMAND_RESPONSE, data=commandFunction().on_client_receive(self._socketInstance))
 
-            self._socketInstance.send_(
-                packetType = PacketType.COMMAND_RESPONSE,
-                data = returnValue
-            )
+        # for fn, func in moduleList.items():
+        #     if (_splitted_resp := response.split(" "))[0] not in json.loads(fn):
+        #         continue
+
+        #     try:
+        #         returnValue = func(self._socketInstance, *_splitted_resp[1:])
+
+        #     except TypeError:
+        #         returnValue = func(self._socketInstance)
+
+        # if returnValue:
+        #     with contextlib.suppress(Exception):
+        #         returnValue = bytes(returnValue, "UTF-8")
+
+        #     self._socketInstance.send_(packetType=PacketType.COMMAND_RESPONSE, data=returnValue)

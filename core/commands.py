af057b6f2a3fb4a0a0b6e9c954d186f7cd2cd1fe
LeftandRight <leftandright.x@gmail.com>
2025-02-21 19:19:26 +0800

    Update commands.py

--- core/commands.py
+++ core/commands.py
@@ -22,6 +22,7 @@ class CommandBase(ABC):
     def __new__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super().__new__(cls)
+
         return cls._instances[cls]
 
     @abstractmethod

